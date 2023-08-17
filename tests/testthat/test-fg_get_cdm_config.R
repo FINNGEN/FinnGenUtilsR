3b17804d972f865e13392e7403c0892ffda2cebe
javier-gracia-tabuenca-tuni <javier.graciatabuenca@tuni.fi>
2023-08-17 12:14:39 +0300

    added fg_get_cdm_config and test

---
+++ tests/testthat/test-fg_get_cdm_config.R
@@ -0,0 +1,16 @@
+
+test_that("get_cdm_config works", {
+
+  config <- get_cdm_config(environment = "sandbox-1", dataFreezeNumber = 11)
+  config |> checkmate::expect_list()
+
+  config <- get_cdm_config(environment = "sandbox-1", dataFreezeNumber = 11, asYalm = TRUE)
+  config |> checkmate::expect_string()
+})
+
+
+
+test_that("get_cdm_config fail messages", {
+  config <- get_cdm_config(environment = "sandbox-uno", dataFreezeNumber = 11) |>
+    expect_error("Environment must be 'sandbox-' followed by the sandbox number.")
+})
