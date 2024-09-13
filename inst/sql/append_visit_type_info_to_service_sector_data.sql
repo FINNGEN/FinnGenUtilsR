da71fc837cd8f914a094328f9ad46b8d4f6b693f
javier-gracia-tabuenca-tuni <javier.graciatabuenca@tuni.fi>
2024-09-13 13:07:49 +0300

    added options for isClinicVisit and isFollowUp visit

--- inst/sql/append_visit_type_info_to_service_sector_data.sql
+++ inst/sql/append_visit_type_info_to_service_sector_data.sql
@@ -9,6 +9,12 @@
 -- prioritise_SRC_Contact_Urgency_over_SRC_Service_Sector
 -- Some hilmo visits are including both coding systems, SRC|ServiceSector and SRC|Contact|Urgency, if TRUE the second is used
 --
+-- add_is_follow_up_visit :
+-- boolean indicating if a new column should be added to the output table indicating if the visit is a follow up visit
+--
+-- add_is_clinic_visist :
+-- boolean indicating if a new column should be added to the output table indicating if the visit is a clinic visit
+--
 -- new_colums_sufix :
 -- string indicating a prefix to add to the appended columns, default="".
 
@@ -51,8 +57,19 @@ SELECT
   fgc.name_en AS visit_type_name_en@new_colums_sufix,
   fgc.name_fi AS visit_type_name_fi@new_colums_sufix,
   fgc.code AS visit_type_code@new_colums_sufix,
-  fgc.omop_concept_id AS visit_type_omop_concept_id@new_colums_sufix
-FROM visit_type_fg_codes_preprocessed AS ssfgpre
+  fgc.omop_concept_id AS visit_type_omop_concept_id@new_colums_sufix {@add_is_clinic_visist} ? {,
+  CASE
+    WHEN fgc.extra_info = 'clinic_visit' THEN TRUE
+    WHEN fgc.extra_info = 'non_clinic_visit' THEN FALSE
+    ELSE NULL
+  END AS is_clinic_visit@new_colums_sufix} {@add_is_follow_up_visit} ? {,
+  CASE
+    WHEN fgc.name_en IS NULL THEN NULL
+    WHEN fgc.SOURCE NOT IN ('INPAT', 'OUTPAT', 'OPER_IN', 'OPER_OUT', 'PRIM_OUT') THEN NULL
+    WHEN CONTAINS_SUBSTR(fgc.name_en, 'repeat visit') THEN TRUE
+    ELSE FALSE
+  END AS is_follow_up_visit@new_colums_sufix}
+  FROM visit_type_fg_codes_preprocessed AS ssfgpre
 LEFT JOIN @fg_codes_info_table as fgc
 ON ssfgpre.SOURCE IS NOT DISTINCT FROM fgc.SOURCE AND
    ssfgpre.FG_CODE5 IS NOT DISTINCT FROM fgc.FG_CODE5 AND
